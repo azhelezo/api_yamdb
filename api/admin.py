15999a656c4ddd0d6ebda5d0f7fb1e0a61da0ab8
Ирина Егорова <supermegacoolgirlwowyes@gmail.com>
2020-11-02 00:23:30 +0300

    ReviewAdmin and CommentAdmin configured

--- api/admin.py
+++ api/admin.py
@@ -1,1 +1,19 @@
-# add admin
+from django.contrib import admin
+
+from .models import Review, Comment
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'author', 'text')
+    search_fields = ('title', 'author', 'text')
+    empty_value_display = '-пусто-'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'review', 'author', 'text')
+    search_fields = ('review', 'author', 'text')
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Comment, CommentAdmin)
