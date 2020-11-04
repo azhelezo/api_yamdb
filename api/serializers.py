3dfc0ba0be8c96c58d50da35e3a9ad3b60a08a75
Irina Egorova <irinabalerina@Irinas-MacBook-Air.local>
2020-11-04 18:18:08 +0300

    Review, Comment views and serializers updated

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import get_object_or_404
 
 from users.models import User
 
@@ -51,23 +53,32 @@ class TitlePostSerializer(serializers.ModelSerializer):
         model = Title
 
 
-class TitleSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Title
-        fields = '__all__'
-
-
 class ReviewSerializer(serializers.ModelSerializer):
     title = serializers.SlugRelatedField(
         slug_field='name',
         read_only=True,
     )
-
     author = serializers.SlugRelatedField(
+        default=serializers.CurrentUserDefault(),
         slug_field='username',
-        read_only=True,
+        read_only=True
     )
+
+    def validate(self, data):
+        request = self.context['request']
+        author = request.user
+        title_id = self.context['view'].kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        if request.method == 'POST':
+            if Review.objects.filter(title=title, author=author).exists():
+                raise ValidationError('Only one review is allowed')
+        return data
+
+    def create(self, validated_data):
+        author = self.context['request'].user
+        title_id = self.context['view'].kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        return Review.objects.create(title=title, author=author, **validated_data)
 
     class Meta:
         model = Review
@@ -87,3 +98,9 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+
+    def create(self, validated_data):
+        author = self.context['request'].user
+        review_id = self.context['view'].kwargs.get('review_id')
+        review = get_object_or_404(Review, pk=review_id)
+        return Comment.objects.create(review=review, author=author, **validated_data)
--- api/views.py
+++ api/views.py
@@ -4,7 +4,6 @@ from django.db.models import Avg
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, status, viewsets
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import (IsAuthenticated,
@@ -140,27 +139,11 @@ class ReviewViewSet(viewsets.ModelViewSet):
         title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
         return title.reviews.all()
 
-    def perform_create(self, serializer):
-        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
-        if title.reviews.filter(author=self.request.user).exists():
-            raise ValidationError(detail='Only one review is allowed')
-        serializer.save(title=title, author=self.request.user)
-
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [ReviewCommentPermission]
 
     def get_queryset(self):
-        review = get_object_or_404(
-            Review,
-            pk=self.kwargs.get('review_id')
-        )
+        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
         return review.comments.all()
-
-    def perform_create(self, serializer):
-        review = get_object_or_404(
-            Review,
-            pk=self.kwargs.get('review_id')
-        )
-        serializer.save(review=review, author=self.request.user)
